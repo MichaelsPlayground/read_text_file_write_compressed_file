93d2ca17fd4b5a1217133fc6c8255823b89b7a90
michaelfehr <michael_fehrgmx.de>
2021-10-13 10:51:42 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+// main.dart
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // This will be displayed on the screen
+  String _content = ''; // null safety
+
+  // Find the Documents path
+  Future<String> _getDirPath() async {
+    final _dir = await getApplicationDocumentsDirectory();
+    return _dir.path;
+  }
+
+  // This function is triggered when the "Read" button is pressed
+  Future<void> _readData() async {
+    final _dirPath = await _getDirPath();
+    final _myFile = File('$_dirPath/data.txt');
+    final _data = await _myFile.readAsString(encoding: utf8);
+    setState(() {
+      _content = _data;
+    });
+  }
+
+  // TextField controller
+  final _textController = TextEditingController();
+  // This function is triggered when the "Write" buttion is pressed
+  Future<void> _writeData() async {
+    final _dirPath = await _getDirPath();
+
+    final _myFile = File('$_dirPath/data.txt');
+    // If data.txt doesn't exist, it will be created automatically
+
+    await _myFile.writeAsString(_textController.text);
+    _textController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kindacode.com'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(labelText: 'Enter your name'),
+            ),
+            ElevatedButton(
+              child: Text('Save to file'),
+              onPressed: _writeData,
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            Text(
+
+                _content != null
+                    ? _content
+                    : 'Press the button to load your name',
+                style: TextStyle(fontSize: 24, color: Colors.pink)),
+            ElevatedButton(
+              child: Text('Read my name from the file'),
+              onPressed: _readData,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
